d42a5a50f4b01a1c5a4f0968941fcb1d2fc47d32
LaurenceLiZhixin <382673304@qq.com>
2021-09-04 14:23:03 +0800

    fix: add triple attachment

--- cmd/protoc-gen-triple/plugin/triple/triple.go
+++ cmd/protoc-gen-triple/plugin/triple/triple.go
@@ -44,18 +44,18 @@ const (
 )
 
 func init() {
-	generator.RegisterPlugin(new(dubboGrpc))
+	generator.RegisterPlugin(new(triple))
 }
 
-// grpc is an implementation of the Go protocol buffer compiler's
-// plugin architecture.  It generates bindings for gRPC-dubbo support.
-type dubboGrpc struct {
+// triple is an implementation of the Go protocol buffer compiler's
+// plugin architecture.  It generates bindings for gRPC-triple support.
+type triple struct {
 	gen *generator.Generator
 }
 
 // Name returns the name of this plugin, "grpc".
-func (g *dubboGrpc) Name() string {
-	return "dubbo"
+func (g *triple) Name() string {
+	return "triple"
 }
 
 // The names for packages imported in the generated codes.
@@ -67,28 +67,28 @@ var (
 )
 
 // Init initializes the plugin.
-func (g *dubboGrpc) Init(gen *generator.Generator) {
+func (g *triple) Init(gen *generator.Generator) {
 	g.gen = gen
 }
 
 // Given a type name defined in a .proto, return its object.
 // Also record that we're using it, to guarantee the associated import.
-func (g *dubboGrpc) objectNamed(name string) generator.Object {
+func (g *triple) objectNamed(name string) generator.Object {
 	g.gen.RecordTypeUse(name)
 	return g.gen.ObjectNamed(name)
 }
 
 // Given a type name defined in a .proto, return its name as we will print it.
-func (g *dubboGrpc) typeName(str string) string {
+func (g *triple) typeName(str string) string {
 	return g.gen.TypeName(g.objectNamed(str))
 }
 
 // P forwards to g.gen.P.
-func (g *dubboGrpc) P(args ...interface{}) { g.gen.P(args...) }
+func (g *triple) P(args ...interface{}) { g.gen.P(args...) }
 
 // Generate generates codes for the services in the given file.
 // be consistent with grpc plugin
-func (g *dubboGrpc) Generate(file *generator.FileDescriptor) {
+func (g *triple) Generate(file *generator.FileDescriptor) {
 	if len(file.FileDescriptorProto.Service) == 0 {
 		return
 	}
@@ -102,13 +102,14 @@ func (g *dubboGrpc) Generate(file *generator.FileDescriptor) {
 }
 
 // GenerateImports generates the import declaration for this file.
-func (g *dubboGrpc) GenerateImports(file *generator.FileDescriptor) {
+func (g *triple) GenerateImports(file *generator.FileDescriptor) {
 	g.P("import (")
 	g.P(`dgrpc "dubbo.apache.org/dubbo-go/v3/protocol/dubbo3"`)
 	g.P(`"dubbo.apache.org/dubbo-go/v3/protocol/invocation"`)
 	g.P(`"dubbo.apache.org/dubbo-go/v3/protocol"`)
 	g.P(`dubbo3 "github.com/dubbogo/triple/pkg/triple"`)
 	g.P(`tripleConstant "github.com/dubbogo/triple/pkg/common/constant"`)
+	g.P(`"github.com/dubbogo/triple/pkg/common"`)
 	g.P(` ) `)
 }
 
@@ -119,9 +120,8 @@ func unexport(s string) string { return strings.ToLower(s[:1]) + s[1:] }
 var deprecationComment = "// Deprecated: Do not use."
 
 // generateService generates all the codes for the named service.
-func (g *dubboGrpc) generateService(file *generator.FileDescriptor, service *pb.ServiceDescriptorProto, index int) {
+func (g *triple) generateService(file *generator.FileDescriptor, service *pb.ServiceDescriptorProto, index int) {
 	path := fmt.Sprintf("6,%d", index) // 6 means service.
-
 	origServName := service.GetName()
 	fullServName := origServName
 	if pkg := file.GetPackage(); pkg != "" {
@@ -145,10 +145,10 @@ func (g *dubboGrpc) generateService(file *generator.FileDescriptor, service *pb.
 		outputTypeName := outputTypeNames[len(outputTypeNames)-1]
 		if method.GetServerStreaming() || method.GetClientStreaming() {
 			//now we only support two way streaming
-			g.P(fmt.Sprintf("func (c *%sDubbo3Client) %s(ctx %s.Context,opt ...grpc.CallOption) (%s, error) {",
+			g.P(fmt.Sprintf("func (c *%sDubbo3Client) %s(ctx %s.Context,opts ...grpc.CallOption) (%s, error) {",
 				lowerServName, method.GetName(), contextPkg, servName+"_"+method.GetName()+"Client"))
 			g.P(fmt.Sprintf("interfaceKey := ctx.Value(tripleConstant.InterfaceKey).(string)"))
-			g.P(fmt.Sprintf("stream, err := c.cc.NewStream(ctx, \"/\" + interfaceKey + \"/%s\", opt...)", method.GetName()))
+			g.P(fmt.Sprintf("stream, err := c.cc.NewStream(ctx, \"/\" + interfaceKey + \"/%s\", opts...)", method.GetName()))
 			g.P("if err != nil {")
 			g.P("return nil, err")
 			g.P("}")
@@ -158,15 +158,11 @@ func (g *dubboGrpc) generateService(file *generator.FileDescriptor, service *pb.
 			continue
 		}
 		// unary rpc method client
-		g.P(fmt.Sprintf("func (c *%sDubbo3Client) %s(ctx %s.Context, in *%s, opt ...grpc.CallOption) (*%s, error) {",
+		g.P(fmt.Sprintf("func (c *%sDubbo3Client) %s(ctx %s.Context, in *%s, opts ...grpc.CallOption) (*%s, common.ErrorWithAttachment) {",
 			lowerServName, method.GetName(), contextPkg, inputTypeName, outputTypeName))
 		g.P(fmt.Sprintf("out := new(%s)", outputTypeName))
 		g.P(fmt.Sprintf("interfaceKey := ctx.Value(tripleConstant.InterfaceKey).(string)"))
-		g.P(fmt.Sprintf("err := c.cc.Invoke(ctx, \"/\" + interfaceKey + \"/%s\", in, out)", method.GetName()))
-		g.P("if err != nil {")
-		g.P("return nil, err")
-		g.P("}")
-		g.P("return out, nil")
+		g.P(fmt.Sprintf("return out, c.cc.Invoke(ctx, \"/\" + interfaceKey + \"/%s\", in, out)", method.GetName()))
 		g.P("}")
 	}
 
@@ -182,6 +178,19 @@ func (g *dubboGrpc) generateService(file *generator.FileDescriptor, service *pb.
 		g.P("//")
 		g.P(deprecationComment)
 	}
+
+	g.P("type ", servName, "Client interface {")
+	for i, method := range service.Method {
+		g.gen.PrintComments(fmt.Sprintf("%s,2,%d", path, i)) // 2 means method in a service.
+		if method.GetOptions().GetDeprecated() {
+			g.P("//")
+			g.P(deprecationComment)
+		}
+		g.P(g.generateTripleClientSignature(servName, method))
+	}
+	g.P("}")
+	g.P()
+
 	dubboSrvName := servName + "ClientImpl"
 	g.P("type ", dubboSrvName, " struct {")
 	for i, method := range service.Method {
@@ -198,6 +207,23 @@ func (g *dubboGrpc) generateService(file *generator.FileDescriptor, service *pb.
 	// NewClient factory.
 	if deprecated {
 		g.P(deprecationComment)
+	}
+
+	var methodIndex, streamIndex int
+	serviceDescVar := "_" + servName + "_serviceDesc"
+	// Client method implementations.
+	for _, method := range service.Method {
+		var descExpr string
+		if !method.GetServerStreaming() && !method.GetClientStreaming() {
+			// Unary RPC method
+			descExpr = fmt.Sprintf("&%s.Methods[%d]", serviceDescVar, methodIndex)
+			methodIndex++
+		} else {
+			// Streaming RPC method
+			descExpr = fmt.Sprintf("&%s.Streams[%d]", serviceDescVar, streamIndex)
+			streamIndex++
+		}
+		g.generateClientMethod(servName, fullServName, serviceDescVar, method, descExpr)
 	}
 
 	// add Reference method
@@ -219,8 +245,26 @@ func (g *dubboGrpc) generateService(file *generator.FileDescriptor, service *pb.
 	g.P()
 
 	// Server interface.
-	serverType := servName + "ProviderBase"
-	g.P("type ", serverType, " struct {")
+	serverType := servName + "Server"
+	g.P("// ", serverType, " is the server API for ", servName, " service.")
+	if deprecated {
+		g.P("//")
+		g.P(deprecationComment)
+	}
+	g.P("type ", serverType, " interface {")
+	for i, method := range service.Method {
+		g.gen.PrintComments(fmt.Sprintf("%s,2,%d", path, i)) // 2 means method in a service.
+		if method.GetOptions().GetDeprecated() {
+			g.P("//")
+			g.P(deprecationComment)
+		}
+		g.P(g.generateTripleServerSignature(servName, method))
+	}
+	g.P("}")
+	g.P()
+
+	tripleServerBaseType := servName + "ProviderBase"
+	g.P("type ", tripleServerBaseType, " struct {")
 	g.P("proxyImpl protocol.Invoker")
 	g.P("}")
 	g.P()
@@ -229,19 +273,19 @@ func (g *dubboGrpc) generateService(file *generator.FileDescriptor, service *pb.
 	//func (g *GreeterProviderBase) SetProxyImpl(impl protocol.Invoker) {
 	//	g.proxyImpl = impl
 	//}
-	g.P("func (s *", serverType, ") SetProxyImpl(impl protocol.Invoker) {")
+	g.P("func (s *", tripleServerBaseType, ") SetProxyImpl(impl protocol.Invoker) {")
 	g.P(`s.proxyImpl = impl`)
 	g.P("}")
 	g.P()
 
 	// return get method
-	g.P("func (s *", serverType, ") GetProxyImpl() protocol.Invoker {")
+	g.P("func (s *", tripleServerBaseType, ") GetProxyImpl() protocol.Invoker {")
 	g.P(`return s.proxyImpl`)
 	g.P("}")
 	g.P()
 
 	// return reference
-	g.P("func (c *", serverType, ") ", " Reference() string ", "{")
+	g.P("func (c *", tripleServerBaseType, ") ", " Reference() string ", "{")
 	g.P(`return "`, unexport(servName), `Impl"`)
 	g.P("}")
 	g.P()
@@ -255,7 +299,7 @@ func (g *dubboGrpc) generateService(file *generator.FileDescriptor, service *pb.
 
 	grpcserverType := servName + "Server"
 	// return service desc
-	g.P("func (s *", serverType, ") ServiceDesc() *grpc.ServiceDesc {")
+	g.P("func (s *", tripleServerBaseType, ") ServiceDesc() *grpc.ServiceDesc {")
 	g.P(`return &grpc.ServiceDesc{`)
 	g.P("ServiceName: ", strconv.Quote(fullServName), ",")
 	g.P("HandlerType: (*", grpcserverType, ")(nil),")
@@ -293,8 +337,27 @@ func (g *dubboGrpc) generateService(file *generator.FileDescriptor, service *pb.
 	g.P()
 }
 
+// generateTripleClientSignature returns the client-side signature for a method.
+func (g *triple) generateTripleClientSignature(servName string, method *pb.MethodDescriptorProto) string {
+	origMethName := method.GetName()
+	methName := generator.CamelCase(origMethName)
+	if reservedClientName[methName] {
+		methName += "_"
+	}
+	reqArg := ", in *" + g.typeName(method.GetInputType())
+	if method.GetClientStreaming() {
+		reqArg = ""
+	}
+	respName := "*" + g.typeName(method.GetOutputType())
+	if method.GetServerStreaming() || method.GetClientStreaming() {
+		respName = servName + "_" + generator.CamelCase(origMethName) + "Client"
+		return fmt.Sprintf("%s(ctx %s.Context%s, opts ...%s.CallOption) (%s, error)", methName, contextPkg, reqArg, grpcPkg, respName)
+	}
+	return fmt.Sprintf("%s(ctx %s.Context%s, opts ...%s.CallOption) (%s, common.ErrorWithAttachment)", methName, contextPkg, reqArg, grpcPkg, respName)
+}
+
 // generateClientSignature returns the client-side signature for a method.
-func (g *dubboGrpc) generateClientSignature(servName string, method *pb.MethodDescriptorProto) string {
+func (g *triple) generateClientSignature(servName string, method *pb.MethodDescriptorProto) string {
 	origMethName := method.GetName()
 	methName := generator.CamelCase(origMethName)
 	//if reservedClientName[methName] {
@@ -312,12 +375,93 @@ func (g *dubboGrpc) generateClientSignature(servName string, method *pb.MethodDe
 	return fmt.Sprintf("%s func(ctx %s.Context%s) (*%s, error)", methName, contextPkg, reqArg, g.typeName(method.GetOutputType()))
 }
 
-func (g *dubboGrpc) generateClientMethod(servName, fullServName, serviceDescVar string, method *pb.MethodDescriptorProto, descExpr string) {
+// reservedClientName records whether a client name is reserved on the client side.
+var reservedClientName = map[string]bool{
+	// TODO: do we need any in gRPC?
 }
 
-func (g *dubboGrpc) generateServerMethod(servName, fullServName string, method *pb.MethodDescriptorProto) string {
+//generateTripleServerSignature returns the server-side signature for a method.
+func (g *triple) generateTripleServerSignature(servName string, method *pb.MethodDescriptorProto) string {
+	origMethName := method.GetName()
+	methName := generator.CamelCase(origMethName)
+	if reservedClientName[methName] {
+		methName += "_"
+	}
+
+	var reqArgs []string
+	ret := "error"
+	if !method.GetServerStreaming() && !method.GetClientStreaming() {
+		reqArgs = append(reqArgs, contextPkg+".Context")
+		ret = "(*" + g.typeName(method.GetOutputType()) + ", error)"
+	}
+	if !method.GetClientStreaming() {
+		reqArgs = append(reqArgs, "*"+g.typeName(method.GetInputType()))
+	}
+	if method.GetServerStreaming() || method.GetClientStreaming() {
+		reqArgs = append(reqArgs, servName+"_"+generator.CamelCase(origMethName)+"Server")
+	}
+
+	return methName + "(" + strings.Join(reqArgs, ", ") + ") " + ret
+}
+
+func (g *triple) generateClientMethod(servName, fullServName, serviceDescVar string, method *pb.MethodDescriptorProto, descExpr string) {
 	methName := generator.CamelCase(method.GetName())
-	hname := fmt.Sprintf("_DUBBO_%s_%s_Handler", servName, methName)
+	streamType := unexport(servName) + methName + "Client"
+	inType := g.typeName(method.GetInputType())
+	outType := g.typeName(method.GetOutputType())
+
+	genSend := method.GetClientStreaming()
+	genRecv := method.GetServerStreaming()
+	genCloseAndRecv := !method.GetServerStreaming()
+
+	// Stream auxiliary types and methods.
+	g.P("type ", servName, "_", methName, "Client interface {")
+	if genSend {
+		g.P("Send(*", inType, ") error")
+	}
+	if genRecv {
+		g.P("Recv() (*", outType, ", error)")
+	}
+	if genCloseAndRecv {
+		g.P("CloseAndRecv() (*", outType, ", error)")
+	}
+	g.P(grpcPkg, ".ClientStream")
+	g.P("}")
+	g.P()
+
+	g.P("type ", streamType, " struct {")
+	g.P(grpcPkg, ".ClientStream")
+	g.P("}")
+	g.P()
+
+	if genSend {
+		g.P("func (x *", streamType, ") Send(m *", inType, ") error {")
+		g.P("return x.ClientStream.SendMsg(m)")
+		g.P("}")
+		g.P()
+	}
+	if genRecv {
+		g.P("func (x *", streamType, ") Recv() (*", outType, ", error) {")
+		g.P("m := new(", outType, ")")
+		g.P("if err := x.ClientStream.RecvMsg(m); err != nil { return nil, err }")
+		g.P("return m, nil")
+		g.P("}")
+		g.P()
+	}
+	if genCloseAndRecv {
+		g.P("func (x *", streamType, ") CloseAndRecv() (*", outType, ", error) {")
+		g.P("if err := x.ClientStream.CloseSend(); err != nil { return nil, err }")
+		g.P("m := new(", outType, ")")
+		g.P("if err := x.ClientStream.RecvMsg(m); err != nil { return nil, err }")
+		g.P("return m, nil")
+		g.P("}")
+		g.P()
+	}
+}
+
+func (g *triple) generateServerMethod(servName, fullServName string, method *pb.MethodDescriptorProto) string {
+	methName := generator.CamelCase(method.GetName())
+	hname := fmt.Sprintf("_Triple_%s_%s_Handler", servName, methName)
 	inType := g.typeName(method.GetInputType())
 
 	if !method.GetServerStreaming() && !method.GetClientStreaming() {
@@ -332,7 +476,7 @@ func (g *dubboGrpc) generateServerMethod(servName, fullServName string, method *
 
 		g.P("if interceptor == nil {")
 		g.P("result := base.GetProxyImpl().Invoke(ctx, invo)")
-		g.P("return result.Result(), result.Error()")
+		g.P("return result, result.Error()")
 		g.P("}")
 
 		g.P("info := &", grpcPkg, ".UnaryServerInfo{")
@@ -357,6 +501,7 @@ func (g *dubboGrpc) generateServerMethod(servName, fullServName string, method *
 	g.P(`invo := invocation.NewRPCInvocation("`, methName, `", nil, nil)`)
 	g.P("if !ok {")
 	g.P("fmt.Println(invo)")
+	g.P(`return nil`)
 	g.P("}")
 	if !method.GetClientStreaming() {
 		g.P("m := new(", inType, ")")
@@ -367,6 +512,52 @@ func (g *dubboGrpc) generateServerMethod(servName, fullServName string, method *
 	}
 	g.P("}")
 	g.P()
+
+	outType := g.typeName(method.GetOutputType())
+	genSend := method.GetServerStreaming()
+	genSendAndClose := !method.GetServerStreaming()
+	genRecv := method.GetClientStreaming()
+
+	// Stream auxiliary types and methods.
+	g.P("type ", servName, "_", methName, "Server interface {")
+	if genSend {
+		g.P("Send(*", outType, ") error")
+	}
+	if genSendAndClose {
+		g.P("SendAndClose(*", outType, ") error")
+	}
+	if genRecv {
+		g.P("Recv() (*", inType, ", error)")
+	}
+	g.P(grpcPkg, ".ServerStream")
+	g.P("}")
+	g.P()
+
+	g.P("type ", streamType, " struct {")
+	g.P(grpcPkg, ".ServerStream")
+	g.P("}")
+	g.P()
+
+	if genSend {
+		g.P("func (x *", streamType, ") Send(m *", outType, ") error {")
+		g.P("return x.ServerStream.SendMsg(m)")
+		g.P("}")
+		g.P()
+	}
+	if genSendAndClose {
+		g.P("func (x *", streamType, ") SendAndClose(m *", outType, ") error {")
+		g.P("return x.ServerStream.SendMsg(m)")
+		g.P("}")
+		g.P()
+	}
+	if genRecv {
+		g.P("func (x *", streamType, ") Recv() (*", inType, ", error) {")
+		g.P("m := new(", inType, ")")
+		g.P("if err := x.ServerStream.RecvMsg(m); err != nil { return nil, err }")
+		g.P("return m, nil")
+		g.P("}")
+		g.P()
+	}
 
 	return hname
 }
