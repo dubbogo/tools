f963e56de4946d8b496f0320bfc1b9f94d884e26
LaurenceLiZhixin <382673304@qq.com>
2021-10-29 19:35:49 +0800

    fix: update protoc-gen-go-triple

---
+++ cmd/protoc-gen-go-triple/main.go
@@ -0,0 +1,50 @@
+// protoc-gen-go-triple is a plugin for the Google protocol buffer compiler to
+// generate Go code. Install it by building this program and making it
+// accessible within your PATH with the name:
+//	protoc-gen-go-triple
+//
+// The 'go-grpc' suffix becomes part of the argument for the protocol compiler,
+// such that it can be invoked as:
+//	protoc --go-triple_out=. path/to/file.proto
+//
+// This generates Go service definitions for the protocol buffer defined by
+// file.proto.  With that input, the output will be written to:
+//	path/to/file_triple.pb.go
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+import (
+	"github.com/dubbogo/tools/cmd/protoc-gen-go-triple/triple"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+func main() {
+	showVersion := flag.Bool("version", false, "print the version and exit")
+	flag.Parse()
+	if *showVersion {
+		fmt.Printf("protoc-gen-go-triple %v\n", triple.Version)
+		return
+	}
+
+	var flags flag.FlagSet
+	triple.RequireUnimplemented = flags.Bool("require_unimplemented_servers", true, "set to false to match legacy behavior")
+
+	protogen.Options{
+		ParamFunc: flags.Set,
+	}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+		for _, f := range gen.Files {
+			if !f.Generate {
+				continue
+			}
+			triple.GenerateFile(gen, f)
+		}
+		return nil
+	})
+}
--- go.mod
+++ go.mod
@@ -3,13 +3,14 @@ module github.com/dubbogo/tools
 go 1.13
 
 require (
-	dubbo.apache.org/dubbo-go/v3 v3.0.0-rc2.0.20210904062533-3e0412eab250
-	github.com/apache/dubbo-go-hessian2 v1.9.2
-	github.com/dubbogo/gost v1.11.16
-	github.com/dubbogo/triple v1.0.6-0.20210904050749-5721796f3fd6
+	dubbo.apache.org/dubbo-go/v3 v3.0.0-rc2.0.20211029104429-bfc60c2496f9
+	github.com/apache/dubbo-go-hessian2 v1.9.3
+	github.com/dubbogo/gost v1.11.19
+	github.com/dubbogo/triple v1.0.9-0.20211029101533-26805fcfcf62
 	github.com/golang/protobuf v1.5.2
 	github.com/magiconair/properties v1.8.5
 	github.com/pkg/errors v0.9.1
 	go.uber.org/atomic v1.7.0
-	google.golang.org/grpc v1.38.0
+	google.golang.org/grpc v1.41.0
+	google.golang.org/protobuf v1.27.1
 )
