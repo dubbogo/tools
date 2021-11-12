7797df034c0651591a8341d388e6d62411afd26e
LaurenceLiZhixin <382673304@qq.com>
2021-11-12 13:14:45 +0800

    fix: add grpc tools

---
+++ cmd/grpc/grpc_protocol.go
@@ -0,0 +1,122 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package grpc
+
+import (
+	"strconv"
+	"sync"
+)
+
+import (
+	"dubbo.apache.org/dubbo-go/v3/common"
+	"dubbo.apache.org/dubbo-go/v3/common/constant"
+	"dubbo.apache.org/dubbo-go/v3/common/extension"
+	"dubbo.apache.org/dubbo-go/v3/common/logger"
+	"dubbo.apache.org/dubbo-go/v3/protocol"
+)
+
+const (
+	// GRPC module name
+	GRPC = "grpc"
+)
+
+func init() {
+	extension.SetProtocol(GRPC, GetProtocol)
+}
+
+var grpcProtocol *GrpcProtocol
+
+// GrpcProtocol is gRPC protocol
+type GrpcProtocol struct {
+	protocol.BaseProtocol
+	serverMap  map[string]*Server
+	serverLock sync.Mutex
+}
+
+// NewGRPCProtocol creates new gRPC protocol
+func NewGRPCProtocol() *GrpcProtocol {
+	return &GrpcProtocol{
+		BaseProtocol: protocol.NewBaseProtocol(),
+		serverMap:    make(map[string]*Server),
+	}
+}
+
+// Export gRPC service for remote invocation
+func (gp *GrpcProtocol) Export(invoker protocol.Invoker) protocol.Exporter {
+	url := invoker.GetURL()
+	serviceKey := url.ServiceKey()
+	exporter := NewGrpcExporter(serviceKey, invoker, gp.ExporterMap())
+	gp.SetExporterMap(serviceKey, exporter)
+	logger.Infof("Export service: %s", url.String())
+	gp.openServer(url)
+	return exporter
+}
+
+func (gp *GrpcProtocol) openServer(url *common.URL) {
+	gp.serverLock.Lock()
+	defer gp.serverLock.Unlock()
+
+	if _, ok := gp.serverMap[url.Location]; ok {
+		return
+	}
+
+	if _, ok := gp.ExporterMap().Load(url.ServiceKey()); !ok {
+		panic("[GrpcProtocol]" + url.Key() + "is not existing")
+	}
+
+	grpcMessageSize, _ := strconv.Atoi(url.GetParam(constant.MessageSizeKey, "4"))
+	srv := NewServer()
+	srv.SetBufferSize(grpcMessageSize)
+	gp.serverMap[url.Location] = srv
+	srv.Start(url)
+}
+
+// Refer a remote gRPC service
+func (gp *GrpcProtocol) Refer(url *common.URL) protocol.Invoker {
+	client, err := NewClient(url)
+	if err != nil {
+		logger.Warnf("can't dial the server: %s", url.Key())
+		return nil
+	}
+	invoker := NewGrpcInvoker(url, client)
+	gp.SetInvokers(invoker)
+	logger.Infof("Refer service: %s", url.String())
+	return invoker
+}
+
+// Destroy will destroy gRPC all invoker and exporter, so it only is called once.
+func (gp *GrpcProtocol) Destroy() {
+	logger.Infof("GrpcProtocol destroy.")
+
+	gp.BaseProtocol.Destroy()
+
+	gp.serverLock.Lock()
+	defer gp.serverLock.Unlock()
+	for key, server := range gp.serverMap {
+		delete(gp.serverMap, key)
+		server.Stop()
+	}
+}
+
+// GetProtocol gets gRPC protocol, will create if null.
+func GetProtocol() protocol.Protocol {
+	if grpcProtocol == nil {
+		grpcProtocol = NewGRPCProtocol()
+	}
+	return grpcProtocol
+}
--- go.mod
+++ go.mod
@@ -3,14 +3,18 @@ module github.com/dubbogo/tools
 go 1.13
 
 require (
-	dubbo.apache.org/dubbo-go/v3 v3.0.0-rc2.0.20211029104429-bfc60c2496f9
+	dubbo.apache.org/dubbo-go/v3 v3.0.0-rc4-1
 	github.com/apache/dubbo-go-hessian2 v1.9.3
 	github.com/dubbogo/gost v1.11.19
-	github.com/dubbogo/triple v1.0.9-0.20211029101533-26805fcfcf62
+	github.com/dubbogo/triple v1.1.3
 	github.com/golang/protobuf v1.5.2
+	github.com/grpc-ecosystem/grpc-opentracing v0.0.0-20180507213350-8e809c8a8645
 	github.com/magiconair/properties v1.8.5
+	github.com/opentracing/opentracing-go v1.2.0
 	github.com/pkg/errors v0.9.1
-	go.uber.org/atomic v1.7.0
-	google.golang.org/grpc v1.41.0
+	github.com/stretchr/testify v1.7.0
+	go.uber.org/atomic v1.9.0
+	google.golang.org/grpc v1.42.0
 	google.golang.org/protobuf v1.27.1
+	gopkg.in/yaml.v2 v2.4.0
 )
